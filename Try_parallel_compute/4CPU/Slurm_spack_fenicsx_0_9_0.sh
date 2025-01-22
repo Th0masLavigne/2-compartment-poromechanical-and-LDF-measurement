7c2e44db24d81d25c122eb518096a874cf64ca9e
Th0masLavigne <lavignethomas@hotmail.fr>
2025-01-22 15:29:40 +0100

    update

--- Try_parallel_compute/4CPU/Slurm_spack_fenicsx_0_9_0.sh
+++ Try_parallel_compute/4CPU/Slurm_spack_fenicsx_0_9_0.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=4CPU
+#SBATCH --job-name=ID_63_refine_para
 #SBATCH --partition=CPU_Compute
-#SBATCH --ntasks=4
+#SBATCH --ntasks=8
 #SBATCH --nodes=1
 ##SBATCH --tasks-per-node=1
-#SBATCH --mem-per-cpu=16GB
-##SBATCH --mem=0
+##SBATCH --mem-per-cpu=32GB
+#SBATCH --mem=0
 #
 ## Suggested batch arguments
 ##SBATCH --mail-type=ALL
@@ -20,5 +20,6 @@
 . /modules/spack/v0.23/share/spack/setup-env.sh
 spack env activate tlavigne
 
-srun python main_finger.py
+# srun python main_finger.py
+mpirun -n $SLURM_NTASKS python main_finger.py
 
--- Try_parallel_compute/8CPU/Slurm_spack_fenicsx_0_9_0.sh
+++ Try_parallel_compute/8CPU/Slurm_spack_fenicsx_0_9_0.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=8CPU
+#SBATCH --job-name=ID_63_refine_para
 #SBATCH --partition=CPU_Compute
 #SBATCH --ntasks=8
 #SBATCH --nodes=1
 ##SBATCH --tasks-per-node=1
-##SBATCH --mem-per-cpu=16GB
+##SBATCH --mem-per-cpu=32GB
 #SBATCH --mem=0
 #
 ## Suggested batch arguments
@@ -20,5 +20,6 @@
 . /modules/spack/v0.23/share/spack/setup-env.sh
 spack env activate tlavigne
 
-srun python main_finger.py
+# srun python main_finger.py
+mpirun -n $SLURM_NTASKS python main_finger.py
 
