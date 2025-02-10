1f6648574dab7c6535925b61ed2ebc5e8330dca6
Th0masLavigne <lavignethomas@hotmail.fr>
2025-02-10 12:05:25 +0100

    update

--- Parallel_Install_FEniCSx/speedup/speedup.py
+++ Parallel_Install_FEniCSx/speedup/speedup.py
@@ -15,10 +15,12 @@ data = pd.read_csv('speedup.dat', delimiter=" ")
 data["Time"]=data['Time'].apply(convert_to_seconds)
 data['Speedup'] = data['Time'].iloc[0] / data['Time']
 
-
-plt.plot(data['CPU'], data['CPU'], label="ideal")
-plt.plot(data['CPU'], data['Speedup'], 'o-', label="real")
-plt.xlabel('Number of CPU')
-plt.ylabel('speedup')
+plt.rcParams.update({'font.size': 25})
+plt.figure(figsize=(20, 12))
+plt.plot(data['CPU'], data['CPU'], label="ideal", linewidth=3, color="gray")
+plt.plot(data['CPU'], data['Speedup'], 'o-', label="real", linewidth=3, markersize=30, color="blue")
+plt.xlabel('Number of CPUs [-]')
+plt.ylabel('Speed-up [-]')
 plt.legend()
-plt.show()
+plt.grid()
+plt.savefig('speedup.jpg', bbox_inches='tight')
