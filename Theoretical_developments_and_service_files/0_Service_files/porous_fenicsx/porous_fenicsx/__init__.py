8d66d99300eb0a325d30e48618031185d462d892
Th0masLavigne <lavignethomas@hotmail.fr>
2025-02-18 09:29:46 +0100

    update package

--- Theoretical_developments_and_service_files/0_Service_files/porous_fenicsx/porous_fenicsx/__init__.py
+++ Theoretical_developments_and_service_files/0_Service_files/porous_fenicsx/porous_fenicsx/__init__.py
@@ -1,3 +1,6 @@
-print("Le package 'Porous_media_package' a été chargé")
+from .constitutive_laws as constitutive_laws
 
 __all__ = ["constitutive_laws", "functions", "variational_forms",  "variational_forms_TW", "variational_forms_Neo_Hooke"]
+
+if __name__=='__main__':
+	print("The package 'Porous_media_package' was successfully loaded")
